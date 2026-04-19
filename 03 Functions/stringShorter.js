27d1f8d5aef0045214a283079ef32ab6491c7344
Sachin Pal <sch99651@gmail.com>
2026-04-19 17:34:12 +0530

    Delete 03 Functions/stringShorter.js

--- 03 Functions/stringShorter.js
+++
@@ -1,8 +0,0 @@
-function truncateString(str, num) {
-  if (str.length > num) {
-    return str.slice(0, num) + "...";
-  } else {
-    return str;
-  }
-}
-console.log(truncateString("Absolutely Longer", 2));
