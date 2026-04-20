5a07a53629e67f48c5b7cd51d0d14fcdabc3dbe4
Sachin Pal <sch99651@gmail.com>
2026-04-20 21:53:40 +0530

    adding Leap year calculator

---
+++ 03 Functions/LeapYearCalculator.js
@@ -0,0 +1,8 @@
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+console.log(truncateString("Absolutely Longer", 2));
