b0b139422050db5bfbee12383518a6606580a83b
Sachin Pal <sch99651@gmail.com>
2026-04-27 20:26:14 +0530

    adding a loop lab file

---
+++ 06 Loops/chunkyMonkeyAlgorithm.js
@@ -0,0 +1,11 @@
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  
+  for (let i = 0; i < arr.length; i += size) {(arr.slice(i, i + size));
+  }
+  
+  return newArr;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
