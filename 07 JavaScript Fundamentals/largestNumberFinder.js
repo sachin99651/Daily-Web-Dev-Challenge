915ea3ae9e961f741aef7c7ee964fbf46473b728
Sachin Pal <sch99651@gmail.com>
2026-05-07 18:13:49 +0530

    adding a new js fundament chapter lab where find the largest number

---
+++ 07 JavaScript Fundamentals/largestNumberFinder.js
@@ -0,0 +1,17 @@
+function largestOfAll(arr) {
+  const results = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+
+    results.push(largestNumber);
+  }
+
+  return results;
+}
