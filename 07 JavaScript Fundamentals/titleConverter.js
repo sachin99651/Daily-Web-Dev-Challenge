dc17c93f9b1b87d8520f201aa17f7014efa9b63f
Sachin Pal <sch99651@gmail.com>
2026-05-09 10:57:54 +0530

    Implement titleCase function for string formatting
    Add a function to convert a string to title case.

--- 07 JavaScript Fundamentals/titleConverter.js
+++ 07 JavaScript Fundamentals/titleConverter.js
@@ -1,1 +1,14 @@
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => {
+      
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+     .join(' ');
+}
+
+console.log(titleCase("I like to code"));      // Output: "I Like To Code"
+console.log(titleCase("javaScript is fun"));   // Output: "Javascript Is Fun"
 
