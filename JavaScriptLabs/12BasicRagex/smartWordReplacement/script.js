791f0ee3c7be34fa5aa11fdcb34e849d58d79065
Sachin Pal <sch99651@gmail.com>
2026-06-09 14:39:39 +0530

    adding a new carriculum of Basic Regex chapter

---
+++ JavaScriptLabs/12BasicRagex/smartWordReplacement/script.js
@@ -0,0 +1,9 @@
+function myReplace(str, before, after) {
+  if (before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
+
+  return str.replace(before, after);
+}
